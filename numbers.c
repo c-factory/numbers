d0baee557fc759cf4543b828d3bec4b55b94103b
Ivan Kniazkov <ivan@kniazkov.com>
2020-10-21 15:44:10 +0300

    Decomposing real numbers

--- numbers.c
+++ numbers.c
@@ -7,6 +7,9 @@
 #include "numbers.h"
 #include <memory.h>
 #include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdlib.h>
 
 void init_number_by_integer(number_t *n, integer_t value)
 {
@@ -25,6 +28,82 @@ void init_number_by_integer(number_t *n, integer_t value)
     n->fract_part = 0;
     n->precision = 0;
     n->exponent = 0;
+}
+
+static int max_precision = 0;
+
+int get_max_integer_precision()
+{
+    if (!max_precision)
+        max_precision = (int)log10f(LLONG_MAX);
+    return max_precision;
+}
+
+void init_number_by_real(number_t *n, real_t value)
+{
+    integer_t int_value = (integer_t)value;
+    if (int_value == value)
+    {
+        init_number_by_integer(n, int_value);
+        return;
+    }
+
+    n->real_value = value;
+    n->is_number = true;
+    if (value < 0)
+    {
+        n->is_negative = true;
+        value = -value;
+    }
+    else
+    {
+        n->is_negative = false;
+    }
+
+    int exp2;
+    frexpl(value, &exp2);
+    n->exponent = (int)log10l(pow_2(exp2));
+    if (n->exponent)
+    {
+        real_t new_value = value / pow_10(n->exponent);
+        if (new_value < 1)
+        {
+            n->exponent--;
+            value /= pow_10(n->exponent);
+        }
+        else
+        {
+            value = new_value;
+            while (value > LLONG_MAX)
+            {
+                n->exponent++;
+                value /= 10;
+            }
+        }
+    }
+
+    n->int_part = (integer_t)value;
+    real_t fract_part_real = (value - (integer_t)value);
+    if (fract_part_real)
+    {
+        int precision = get_max_integer_precision() - 3;
+        integer_t fract_part_int = (integer_t)(fract_part_real * pow_10(precision));
+        if (fract_part_int)
+        {
+            if (fract_part_int % 10 > 4)
+                fract_part_int++;
+            while(precision && fract_part_int % 10 == 0)
+            {
+                fract_part_int /= 10;
+                precision--;
+            }
+            n->precision = precision;
+            n->fract_part = fract_part_int;
+            return;
+        }
+    }
+    n->precision = 0;
+    n->fract_part = 0;
 }
 
 static __inline bool is_number(char c)
@@ -72,9 +151,9 @@ void parse_number(number_t *n, const char *str)
         } while (is_number(*p));        
     }
 
-    bool exp_neg = false;
     if (*p == 'e' || *p == 'E')
     {
+        bool exp_neg = false;
         *p++;
         if (*p == '+')
         {
@@ -92,6 +171,7 @@ void parse_number(number_t *n, const char *str)
             n->exponent = n->exponent * 10 + *p - '0';
             p++;
         } while(is_number(*p));
+        if (exp_neg) n->exponent = -n->exponent;
     }
 
     while(*p == ' ')
@@ -101,15 +181,7 @@ void parse_number(number_t *n, const char *str)
     n->is_number = true;
     real_t value = (n->int_part + n->fract_part / pow_10(n->precision));
     if (n->is_negative) value = -value;
-    if (exp_neg)
-    {
-        value /= pow_10(n->exponent);
-        n->exponent = -n->exponent;
-    }
-    else
-    {
-        value *= pow_10(n->exponent);
-    }
+    if (n->exponent) value *= pow_10(n->exponent);
     n->real_value = value;
     return;
 
@@ -175,7 +247,90 @@ void print_number(char *s, number_t *n)
     }
 }
 
-static const integer_t pow10_table[] =
+static const integer_t pow_2_table[] =
+{
+    1,
+    2,
+    4,
+    8,
+    16,
+    32,
+    64,
+    128,
+    256,
+    512,
+    1024,
+    2048,
+    4096,
+    8192,
+    16384,
+    32768,
+    65536,
+    131072,
+    262144,
+    524288,
+    1048576,
+    2097152,
+    4194304,
+    8388608,
+    16777216,
+    33554432,
+    67108864,
+    134217728,
+    268435456,
+    536870912,
+    1073741824,
+    2147483648,
+    4294967296,
+    8589934592,
+    17179869184,
+    34359738368,
+    68719476736,
+    137438953472,
+    274877906944,
+    549755813888,
+    1099511627776,
+    2199023255552,
+    4398046511104,
+    8796093022208,
+    17592186044416,
+    35184372088832,
+    70368744177664,
+    140737488355328,
+    281474976710656,
+    562949953421312,
+    1125899906842624,
+    2251799813685248,
+    4503599627370496,
+    9007199254740992,
+    18014398509481984,
+    36028797018963968,
+    72057594037927936,
+    144115188075855872,
+    288230376151711744,
+    576460752303423488,
+    1152921504606846976,
+    2305843009213693952,
+    4611686018427387904
+};
+
+real_t pow_2(int value)
+{
+    if (value < 0)
+        return (real_t)1 / pow_2(-value);
+    if (value < sizeof(pow_2_table) / sizeof(integer_t))
+        return pow_2_table[value];
+    int k = sizeof(pow_2_table) / sizeof(integer_t) - 1;
+    real_t result = pow_2_table[k];
+    while(k < value)
+    {
+        k++;
+        result *= 2;
+    }
+    return result;
+}
+
+static const integer_t pow_10_table[] =
 {
     1,
     10,
@@ -201,11 +356,11 @@ static const integer_t pow10_table[] =
 real_t pow_10(int value)
 {
     if (value < 0)
-        return 0;
-    if (value < sizeof(pow10_table) / sizeof(integer_t))
-        return pow10_table[value];
-    int k = sizeof(pow10_table) / sizeof(integer_t) - 1;
-    real_t result = pow10_table[k];
+        return (real_t)1 / pow_10(-value);
+    if (value < sizeof(pow_10_table) / sizeof(integer_t))
+        return pow_10_table[value];
+    int k = sizeof(pow_10_table) / sizeof(integer_t) - 1;
+    real_t result = pow_10_table[k];
     while(k < value)
     {
         k++;
--- test.c
+++ test.c
@@ -1,12 +1,14 @@
 #include "numbers.h"
+#include <math.h>
 
 int main(void)
 {
     number_t n;
-    real_t *rv = (real_t*)&n;
-    parse_number(&n, "-10.024e+3");
+    init_number_by_real(&n, 13.001);
+
     char buff[64];
     char *s = buff;
     print_number(s, &n);
+
     return 0;
 }
